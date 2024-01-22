a7b132a2acbf57dd1e9932e9c2c8de16a11687f4
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-23 01:06:19 +0800

    docs:add content

--- 13.Classes,This And New Keywords/1.The New Keyword/script.js
+++ 13.Classes,This And New Keywords/1.The New Keyword/script.js
@@ -2,18 +2,18 @@
 //creates a new data type
 
 //OBJECT
-const person1={}; //cara biasa
-const person2=new Object(); //guna "new" keyword
+const person1 = {}; //cara biasa
+const person2 = new Object(); //guna "new" keyword
 
-person1.firstName="Shahirul"; 
-person2.firstName="Shahirul";
+person1.firstName = "Shahirul";
+person2.firstName = "Shahirul";
 
 console.log(person1); //akan print Shahirul
 console.log(person2); // akan print Shahirul
 
 //ARRAY
-const arr1=[1,2,3,]; //cara biasa
-const arr2=new Array(1,2,3);//guna "new" keyword
+const arr1 = [1, 2, 3]; //cara biasa
+const arr2 = new Array(1, 2, 3); //guna "new" keyword
 
 console.log(arr1); //akan print 1,2,3
 console.log(arr2); //akan print 1,2,3
@@ -24,9 +24,8 @@ console.log(arr2); //akan print 1,2,3
 //"new" keyword ni memang kita guna untuk create Date baru je sebab kalau guna "new" keyword untuk data types lain,syntax dia panjang sangat
 //for example
 
-const myDate=new Date("August 11,2025"); 
+const myDate = new Date("September 29,2024");
 //date is a special type of an object that is built in Javascript
 //There is no other way to create date, we have to use the "new" keyword
 
 console.log(myDate.getFullYear());
-
