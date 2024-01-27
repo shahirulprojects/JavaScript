ae77726191b68df3d6b3754cd1398bb70d2454cc
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-28 02:41:59 +0800

    docs:add content

--- 3.Decision Making Code/script.js
+++ 3.Decision Making Code/script.js
@@ -1,18 +1,14 @@
 //LOGIC AND CONTROL FLOW
 
 //1.IF ELSE STATEMENT
-const age=18;
+const age = 21;
 
-if(age>18){
-console.log("You may enter!")
-}
-
-else if(age===18){
-console.log("You just turned 18,welcome!")
-}
-
-else{
-    console.log("Grow up lil bro")
+if (age > 21) {
+  console.log("You may enter!");
+} else if (age === 21) {
+  console.log("You just turned 21,welcome!");
+} else {
+  console.log("Grow up lil bro");
 }
 
 //2.TERNARY OPERATOR
@@ -21,22 +17,21 @@ else{
 // "?" dibaca sebagai "is"
 //so cam condition is true:false
 
-const umur=18;
+const umur = 21;
 
-umur>=18 ? console.log("You can drive"):console.log("You cannot drive");
+umur >= 21 ? console.log("You can drive") : console.log("You cannot drive");
 
 //3.FOR & WHILE LOOP
 
 //FOR LOOP
-for(let i=0;i<10;i++){
-    console.log(i);
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
 
 //WHILE LOOP
-let i=0;
+let i = 0;
 
-while(i<10){
-    console.log(i)
-    i++;
+while (i < 10) {
+  console.log(i);
+  i++;
 }
-
