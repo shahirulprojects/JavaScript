38574b0a4e4782307c16c13f17e61351f5b87237
shahirulprojects <shahiruliimalo@gmail.com>
2024-02-06 00:10:37 +0800

    docs:add content

--- 7.Closure/script.js
+++ 7.Closure/script.js
@@ -1,18 +1,19 @@
 //CLOSURE
-//Basically dia macam still nak hidupkan outer function tu dengan letakkan variable outer function dalam inner function 
+//Basically dia macam still nak hidupkan outer function tu dengan letakkan variable outer function dalam inner function
 
 //Example 1
-const init=()=>{
-    const hobby="Learning Javascript"; //local variable created by init function
+const init = () => {
+  const hobby = "Learning Javascript"; //local variable created by init function
 
-    const displayHobby=()=>{ //displayHobby is the inner function,a closure
-        console.log(hobby); //using a variable created in the parent function
-    }
+  const displayHobby = () => {
+    //displayHobby is the inner function,a closure
+    console.log(hobby); //using a variable created in the parent function
+  };
 
-    return displayHobby;
-}
+  return displayHobby;
+};
 
-const myFunc=init();
+const myFunc = init();
 myFunc();
 
 //Example 1 is actually sama je dengan bawah ni
@@ -24,22 +25,21 @@ myFunc();
 //     }
 //     displayHobby();
 // }
-// init(); 
+// init();
 
 //Example 2
-const outer = () =>{
-    const outerVar="Hello!";
+const outer = () => {
+  const outerVar = "Hello!";
 
-    const inner=()=>{
-        const innerVar="Hi!"
+  const inner = () => {
+    const innerVar = "Hi!";
 
-        console.log(outerVar,innerVar);
-    };
-    return inner;//buat camni sebab kita actually nak acces inner function tu
-}
+    console.log(outerVar, innerVar);
+  };
+  return inner; //buat camni sebab kita actually nak access inner function tu
+};
 
-const innerFn=outer();//kita nak access inner function tu actually tapi sebab dia dalam outer function so kita kena panggil outer functionla en
-                      //bila kita panggil satu function,dia akan return statement.Ha time ni la kita return inner function sebab kita kan actually nak panggil dia
+const innerFn = outer(); //kita nak access inner function tu actually tapi sebab dia dalam outer function so kita kena panggil outer functionla en
+//bila kita panggil satu function,dia akan return statement.Ha time ni la kita return inner function sebab kita kan actually nak panggil dia
 
 innerFn();
-
