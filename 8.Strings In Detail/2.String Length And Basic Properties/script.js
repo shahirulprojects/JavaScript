b2c209f47c47dd635c7c535e17f961e5fd4d41f5
shahirulprojects <shahiruliimalo@gmail.com>
2024-02-13 00:03:25 +0800

    docs:add content

--- 8.Strings In Detail/2.String Length And Basic Properties/script.js
+++ 8.Strings In Detail/2.String Length And Basic Properties/script.js
@@ -1,21 +1,20 @@
 //untuk kira berapa banyak huruf dalam satu string tu
 //do note that dia kira spacebar as 1 unit jugak
-const name1="Shahirul Ikmal";
+const name1 = "Emma";
 console.log(name1.length);
 
 //untuk access certain letter in a string
-const name2="Shahirul Ikmal"
+const name2 = "Emma";
 console.log(name2[0]); //do note that kiraan start dari 0 bukannya 1
 
 //untuk store certain letter yang kita dah access tu
 
 //untuk access first character
-const firstLetter=name2[0];
+const firstLetter = name2[0];
 
 //untuk access last character
 //const lastLetter=name2[13]; boleh buat camni tapi tak efficient sebab katakanla string tu panjang,tak ke pishang nak kira satu2
 //so kita buat camni
 
-const lastLetter=name2[name2.length-1];//boleh adjustla tolak2 ni cam katakanla hang nak second last ke en,ha tolakla 2
-console.log(firstLetter,lastLetter);
-
+const lastLetter = name2[name2.length - 1]; //boleh adjustla tolak2 ni cam katakanla hang nak second last ke en,ha tolakla 2
+console.log(firstLetter, lastLetter);
