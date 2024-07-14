63030e61652c2fb66593f88a5789a8b3ba9d31b1
shahirulprojects <shahiruliimalo@gmail.com>
2024-07-14 16:03:00 +0800

    feat:revision practice

--- 14.Asynchronous JavaScript/3.Callbacks/script.js
+++ 14.Asynchronous JavaScript/3.Callbacks/script.js
@@ -11,7 +11,6 @@
 // },2000);
 //}
 
-
 // const user=fetchUser("Emma");
 // console.log(`Your username is:${user.username}`); //akan dapat undefined sebab kita kena wait 2 seconds sebelum data username tu kena fetch tapi Javascript engine tak tunggu 2 saat tu so dia just assume benda tu undefined
 
@@ -22,20 +21,20 @@
 
 //so camna kita nak buat Javascript tunggu 2 saat dulu baru dia fetch data tu
 //here comes the concept of callback() function
- const fetchUser=(userName,callback)=>{
-    setTimeout(()=> {
-        callback({userName})}
-        ,2000)}
+const fetchUser = (userName, callback) => {
+  setTimeout(() => {
+    callback({ userName });
+  }, 2000);
+};
 
-
-fetchUser("Emma",(user)=>{ //ada 2 parameter dia accept here which is the userName and callback parameter
-    console.log(`Your name is:${user.userName}`); //kita tulis "user.userName" and bukan "user" sebab "user" tu object,kita nak nilai object tu
+fetchUser("Emma", (user) => {
+  //ada 2 parameter dia accept here which is the userName and callback parameter
+  console.log(`Your name is:${user.userName}`); //kita tulis "user.userName" and bukan "user" sebab "user" tu object,kita nak nilai object tu
 });
 
 //here is a breakdown of what's happening
 //okay first bila kita panggil fetchUser tu,dia akan send "Emma" dulu as the userName parameter
 //and then after 2 saat,dia akan pass parameter userName tu ke callback function as a parameter
 //then parameter userName ni akan dihantar ke parameter kedua fetchUser tu which is user
-//and then barula kita boleh console log 
+//and then barula kita boleh console log
 //basically callback function ni bila kita call another function from our function declaration
-
